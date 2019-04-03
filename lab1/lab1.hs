ab301b784b8911376f2631095531fc75078461f9
William Wennerstr?m <william@willeponken.me>
2019-04-03 20:04:51 +0200

    lab1: continue with some type definitions

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -1,3 +1,26 @@
-kSmallestSet = print
+type Size = Int
 
-main = kSmallestSet [-1, 2, -3, 4, -5]
+type Index = Int
+
+type KSet = [Int]
+
+type KSets = [KSet]
+
+type List = [Int]
+
+type PrintSets = [(Size, Index, Index, KSet)]
+
+makePrintSets :: KSets -> PrintSets
+formatPrintSets :: PrintSets -> String
+producePrintSets :: KSets -> String
+producePrintSets = formatPrintSets . makePrintSets
+
+kSmallestSet :: List -> KSets
+kSmallestSet [] = error "empty list has no smallest k-sets"
+kSmallestSet l = [l]
+
+main = do
+  print $ kSmallestSet [-1, 2, -3, 4, -5]
+  print $ kSmallestSet [x * (-1) ^ x | x <- [1 .. 100]] -- Test case 1
+  print $ kSmallestSet [24, -11, -34, 42, -24, 7, -19, 21] -- Test case 2
+  print $ kSmallestSet [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3] -- Test case 3
