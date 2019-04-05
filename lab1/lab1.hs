91514290ca35b999be47d99ea629043c834d1b6b
William Wennerstr?m <william@willeponken.me>
2019-04-05 16:58:32 +0200

    lab1: working algorithm, missing print out

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -2,25 +2,43 @@ type Size = Int
 
 type Index = Int
 
-type KSet = [Int]
+type Set = [Int]
 
-type KSets = [KSet]
+type Sets = [Set]
 
 type List = [Int]
 
-type PrintSets = [(Size, Index, Index, KSet)]
+type PrintSets = [(Size, Index, Index, Set)]
 
-makePrintSets :: KSets -> PrintSets
-formatPrintSets :: PrintSets -> String
-producePrintSets :: KSets -> String
-producePrintSets = formatPrintSets . makePrintSets
+-- makePrintSets :: Sets -> PrintSets
+-- formatPrintSets :: PrintSets -> String
+-- producePrintSets :: Sets -> String
+-- producePrintSets = formatPrintSets . makePrintSets
+sort :: Sets -> Sets
+sort [] = []
+sort (p:xs) = sort l ++ [p] ++ sort r
+  where
+    l = filter (cmp (<) p) xs
+    r = filter (cmp (>=) p) xs
+    cmp c x p = sum p `c` sum x
 
-kSmallestSet :: List -> KSets
-kSmallestSet [] = error "empty list has no smallest k-sets"
-kSmallestSet l = [l]
+sets :: List -> Sets
+sets l
+  | length l > 1 = sets (tail l) ++ aux l
+  | otherwise = [l]
+  where
+    aux s
+      | length s > 1 = aux (init s) ++ [s]
+      | otherwise = [s]
+
+smallest k = take k . sort
+
+kSmallestSet :: Int -> List -> Sets
+kSmallestSet _ [] = error "empty list has no smallest k-sets"
+kSmallestSet k l = smallest k (sets l)
 
 main = do
-  print $ kSmallestSet [-1, 2, -3, 4, -5]
-  print $ kSmallestSet [x * (-1) ^ x | x <- [1 .. 100]] -- Test case 1
-  print $ kSmallestSet [24, -11, -34, 42, -24, 7, -19, 21] -- Test case 2
-  print $ kSmallestSet [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3] -- Test case 3
+  print $ kSmallestSet 3 [-1, 2, -3, 4, -5]
+  print $ kSmallestSet 3 [x * (-1) ^ x | x <- [1 .. 100]] -- Test case 1
+  print $ kSmallestSet 3 [24, -11, -34, 42, -24, 7, -19, 21] -- Test case 2
+  print $ kSmallestSet 3 [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3] -- Test case 3
