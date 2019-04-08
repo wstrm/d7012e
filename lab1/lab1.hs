6852a07783d5747202e5f25ff03f738f50a764b8
William Wennerstr?m <william@willeponken.me>
2019-04-08 17:20:02 +0200

    lab1: refactor to use indices instead

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -4,35 +4,45 @@ type Index = Int
 
 type Set = [Int]
 
-type Sets = [Set]
-
 type List = [Int]
 
-type PrintSets = [(Size, Index, Index, Set)]
+type Sets = [(Size, Index, Index, Set)]
 
 -- makePrintSets :: Sets -> PrintSets
--- formatPrintSets :: PrintSets -> String
--- producePrintSets :: Sets -> String
--- producePrintSets = formatPrintSets . makePrintSets
+-- makePrintSets (x:xs)
+--   | not (null xs) = (sum x, ) ++ makePrintSets xs
+--   | otherwise = []
+--
+-- sort a list of sets using Quicksort.
 sort :: Sets -> Sets
 sort [] = []
 sort (p:xs) = sort l ++ [p] ++ sort r
   where
-    l = filter (cmp (<) p) xs
-    r = filter (cmp (>=) p) xs
-    cmp c x p = sum p `c` sum x
+    l = filter (< p) xs -- (<) compares with the first element of each tuple,
+    r = filter (>= p) xs -- same goes for (>=) as above.
 
+sublist :: Int -> Int -> List -> List
+sublist i j = drop i . take (j + 1)
+
+-- sets produces the different sets that is possible for the provided list and
+-- returns the size, indices and set for each combination.
 sets :: List -> Sets
-sets l
-  | length l > 1 = sets (tail l) ++ aux l
-  | otherwise = [l]
+sets [] = []
+sets l = aux 0 0
   where
-    aux s
-      | length s > 1 = aux (init s) ++ [s]
-      | otherwise = [s]
+    aux i j
+      | j < m = (sum s, i, j, s) : aux i (j + 1)
+      | i < m = aux (i + 1) (i + 1)
+      | otherwise = []
+      where
+        s = sublist i j l
+        m = length l
 
+-- smallest finds the smallest k sets from a list of sets.
+smallest :: Int -> Sets -> Sets
 smallest k = take k . sort
 
+-- kSmallestSet returns the smallest k sets from a list.
 kSmallestSet :: Int -> List -> Sets
 kSmallestSet _ [] = error "empty list has no smallest k-sets"
 kSmallestSet k l = smallest k (sets l)
@@ -42,3 +52,4 @@ main = do
   print $ kSmallestSet 3 [x * (-1) ^ x | x <- [1 .. 100]] -- Test case 1
   print $ kSmallestSet 3 [24, -11, -34, 42, -24, 7, -19, 21] -- Test case 2
   print $ kSmallestSet 3 [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3] -- Test case 3
+  print $ kSmallestSet 0 []
