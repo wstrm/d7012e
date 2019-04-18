6bcbce57084cfe26987f79f6710ae96095a26a2a
William Wennerstr?m <william@optmzr.se>
2019-04-18 10:35:41 +0200

    lab3: add chars

--- lab3/Parser.hs
+++ lab3/Parser.hs
@@ -1,23 +1,41 @@
-module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
-              lit, number, iter, accept, require, token,
-              spaces, word, (-#), (#-)) where
-import Prelude hiding (return, fail)
-import Data.Char
+module Parser
+  ( module CoreParser
+  , T
+  , digit
+  , digitVal
+  , chars
+  , letter
+  , err
+  , lit
+  , number
+  , iter
+  , accept
+  , require
+  , token
+  , spaces
+  , word
+  , (-#)
+  , ( #- )
+  ) where
+
 import CoreParser
+import Data.Char
+import Prelude hiding (fail, return)
+
 infixl 7 -#, #-
 
 type T a = Parser a
 
 err :: String -> Parser a
-err message cs = error (message++" near "++cs++"\n")
+err message cs = error (message ++ " near " ++ cs ++ "\n")
 
 iter :: Parser a -> Parser [a]
 iter m = m # iter m >-> cons ! return []
+
 {- Same as:
 iter m = ((m # (iter m)) >-> cons) ! (return [])
 -}
-
-cons(a, b) = a:b
+cons (a, b) = a : b
 
 (-#) :: Parser a -> Parser b -> Parser b
 m -# n = error "-# not implemented"
@@ -41,16 +59,17 @@ word :: Parser String
 word = token (letter # iter letter >-> cons)
 
 chars :: Int -> Parser String
-chars n =  error "chars not implemented"
+chars 0 = return []
+chars n = (char # chars (n - 1)) >-> cons
 
 accept :: String -> Parser String
-accept w = (token (chars (length w))) ? (==w)
+accept w = token (chars (length w)) ? (== w)
 
 require :: String -> Parser String
-require w  = error "require not implemented"
+require w = error "require not implemented"
 
 lit :: Char -> Parser Char
-lit c = token char ? (==c)
+lit c = token char ? (== c)
 
 digit :: Parser Char
 digit = char ? isDigit
@@ -59,7 +78,7 @@ digitVal :: Parser Integer
 digitVal = digit >-> digitToInt >-> fromIntegral
 
 number' :: Integer -> Parser Integer
-number' n = digitVal #> (\ d -> number' (10*n+d))
-          ! return n
+number' n = digitVal #> (\d -> number' (10 * n + d)) ! return n
+
 number :: Parser Integer
 number = token (digitVal #> number')
