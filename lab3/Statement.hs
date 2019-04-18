0ea0e2f31c9fc47bbaef09f41e0477bca64da90a
William Wennerstr?m <william@optmzr.se>
2019-04-18 14:33:27 +0200

    lab3: auto format {Expr,Statement}.hs

--- lab3/Statement.hs
+++ lab3/Statement.hs
@@ -1,22 +1,35 @@
-module Statement(T, parse, toString, fromString, exec) where
-import Prelude hiding (return, fail)
-import Parser hiding (T)
+module Statement
+  ( T
+  , parse
+  , toString
+  , fromString
+  , exec
+  ) where
+
 import qualified Dictionary
 import qualified Expr
+import Parser hiding (T)
+import Prelude hiding (fail, return)
+
 type T = Statement
-data Statement =
-    Assignment String Expr.T |
-    If Expr.T Statement Statement
-    deriving Show
+
+data Statement
+  = Assignment String
+               Expr.T
+  | If Expr.T
+       Statement
+       Statement
+  deriving (Show)
 
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
+
 buildAss (v, e) = Assignment v e
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-exec (If cond thenStmts elseStmts: stmts) dict input = 
-    if (Expr.value cond dict)>0 
-    then exec (thenStmts: stmts) dict input
-    else exec (elseStmts: stmts) dict input
+exec (If cond thenStmts elseStmts:stmts) dict input =
+  if Expr.value cond dict > 0
+    then exec (thenStmts : stmts) dict input
+    else exec (elseStmts : stmts) dict input
 
 instance Parse Statement where
   parse = error "Statement.parse not implemented"
