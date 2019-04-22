74cdfc281be54bc928e5acfe76dec1d9a80f3f97
William Wennerstr?m <william@optmzr.se>
2019-04-22 15:53:50 +0200

    lab3: add `exec` for all types of statements

--- lab3/Statement.hs
+++ lab3/Statement.hs
@@ -31,29 +31,41 @@ assignment = word #- accept ":=" # Expr.parse #- require ";" >-> build
   where
     build (v, e) = Assignment v e
 
-write = accept "write" -# Expr.parse #- require ";" >-> Write
-
-read = accept "read" -# word #- require ";" >-> Read
-
 skip = accept "skip" #- require ";" >-> const Skip
 
-while = accept "while" -# Expr.parse #- require "do" # parse >-> build
-  where
-    build (e, s) = While e s
+begin = accept "begin" -# iter parse #- require "end" >-> Begin
 
 if' =
   accept "if" -# Expr.parse #- require "then" # parse #- require "else" # parse >->
   build
   where
-    build ((e, v0), v1) = If e v0 v1
+    build ((e, s0), s1) = If e s0 s1
 
-begin = accept "begin" -# iter parse #- require "end" >-> Begin
+while = accept "while" -# Expr.parse #- require "do" # parse >-> build
+  where
+    build (e, s) = While e s
+
+read = accept "read" -# word #- require ";" >-> Read
+
+write = accept "write" -# Expr.parse #- require ";" >-> Write
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-exec (If cond thenStmts elseStmts:stmts) dict input =
+exec [] dict input = []
+exec (Assignment var val:stmts) dict input =
+  exec stmts (Dictionary.insert (var, Expr.value val dict) dict) input
+exec (Skip:stmts) dict input = exec stmts dict input
+exec (Begin stmt:stmts) dict input = exec (stmt ++ stmts) dict input
+exec (If cond then' else':stmts) dict input =
   if Expr.value cond dict > 0
-    then exec (thenStmts : stmts) dict input
-    else exec (elseStmts : stmts) dict input
+    then exec (then' : stmts) dict input
+    else exec (else' : stmts) dict input
+exec (While cond do':stmts) dict input
+  | Expr.value cond dict > 0 = exec (do' : While cond do' : stmts) dict input
+  | otherwise = exec stmts dict input
+exec (Read var:stmts) dict (input:inputs) =
+  exec stmts (Dictionary.insert (var, input) dict) inputs
+exec (Write expr:stmts) dict input =
+  Expr.value expr dict : exec stmts dict input
 
 instance Parse Statement where
   parse = assignment ! skip ! begin ! if' ! while ! read ! write
