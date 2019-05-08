7ff21f39239a09fbc56c7a253dc08b797964a3e2
William Wennerström <william@willeponken.me>
2019-05-08 11:32:26 +0200

    labp2: correctly calculates all the sets and their sizes (and sorts)

--- labp2/labp2_d7012e_wenwil-5.pl
+++ labp2/labp2_d7012e_wenwil-5.pl
@@ -15,11 +15,11 @@ sum([N|Tail], Sum) :-
 % Returns the sublist between two indices (inclusive).
 % sublist :: List -> I -> J -> List
 sublist([_Skip|Tail], I, J, NewList) :-
-	I > 0, I < J,
+	I > 0, I =< J,
 	NextI is I - 1, NextJ is J - 1,
 	sublist(Tail, NextI, NextJ, NewList).
 sublist([Include|Tail], 0, J, NewList) :-
-	0 < J,
+	0 =< J,
 	NextJ is J - 1,
 	NewList = [Include|Rest],
 	sublist(Tail, 0, NextJ, Rest).
@@ -34,49 +34,46 @@ sets(List, Sets) :- aux(List, 0, 0, Sets).
 % sublists.
 % aux :: List -> Int -> Int -> Sets
 aux(List, I, J, Sets) :- % When J < Length(List).
-	length(List, Length), % ISO defined built-in.
-	J < Length,
 	sublist(List, I, J, Sublist),
 	sum(Sublist, Sum),
 	NextJ is J+1,
-	aux(List, I, NextJ, NextSets),
-	Sets = [[Sum, I, J, Sublist]|NextSets].
-aux(List, I, J, Sets) :-
-	length(List, Length),
-	I < Length,
-	sublist(List, I, J, Sublist),
-	sum(Sublist, Sum),
+	aux(List, I, NextJ, NextSets), !,
+	Sets = [set(Sum, I, J, Sublist)|NextSets].
+aux(List, I, _J, NextSets) :-
+	length(List, Length), I < Length,
 	NextI is I+1,
-	aux(List, NextI, NextI, NextSets),
-	Sets = [[Sum, I, J, Sublist]|NextSets].
+	aux(List, NextI, NextI, NextSets), !.
 aux(_List, Same, Same, []).
 
-remove(X,[X|Xs],Xs).
-remove(X,[Y|Xs],[Y|Ys]) :-
-    dif(X,Y),
-    remove(X,Xs,Ys).
+% The sum of a set.
+% setSum :: Set -> Int
+setSum(set(Sum, _, _, _), Sum).
 
-% Smallest sum in a list of sets.
-% smallest :: Sets -> Set
-smallest([Smallest], Smallest).
-smallest([[S1, I1, J1, L1],[S2|_]|Tail], Smallest) :-
-    S1 =< S2,
-    smallest([[S1, I1, J1, L1]|Tail], Smallest).
-smallest([[S1|_],[S2, I2, J2, L2]|Tail], Smallest) :-
-    S1 > S2,
-    smallest([[S2, I2, J2, L2]|Tail], Smallest).
+% Quicksort a list of sum(Sum, I, J, Sublist).
+% quicksort :: Sets -> Sets
+quicksort([], []).
+quicksort([Pivot|Rest], SortedList) :-
+	partition(Pivot, Rest, Left, Right),
+	quicksort(Left, SortedLeft),
+	quicksort(Right, SortedRight),
+	append(SortedLeft, [Pivot|SortedRight], SortedList).
 
-kSmallest(Sets, K, KSets) :-
-	K > 0,
-	NextK is K-1,
-	smallest(Sets, Set),
-	remove(Set, Sets, NextSets),
-	kSmallest(NextSets, NextK, NextSmallest),
-	KSets = [Set|NextSmallest].
-kSmallest([_Skip|Tail], K, KSets) :-
-	K > 0,
-	NextK is K-1,
-	kSmallest(Tail, NextK, KSets).
-kSmallest(Sets, _, Sets).
+% Partition a list of sum(Sum, I, J, Sublist) into two recursively at a pivot.
+% partition :: Set -> Sets -> Sets -> Sets
+partition(_Pivot, [], [], []).
+partition(Pivot, [Head|Tail], [Head|SortedLeft], SortedRight) :-
+	setSum(Pivot, PivotSum), setSum(Head, HeadSum),
+	HeadSum =< PivotSum,
+	partition(Pivot, Tail, SortedLeft, SortedRight).
+partition(Pivot, [Head|Tail], SortedLeft, [Head|SortedRight]) :-
+	setSum(Pivot, PivotSum), setSum(Head, HeadSum),
+	HeadSum  > PivotSum,
+	partition(Pivot, Tail, SortedLeft, SortedRight).
+
+% kSmallestSets :: Int -> List -> Sets
+kSmallestSets(_K, List, KSets) :-
+	sets(List, Sets),
+	quicksort(Sets, SortedSets),
+	KSets = SortedSets.
 
 /* vim: set syntax=prolog */
