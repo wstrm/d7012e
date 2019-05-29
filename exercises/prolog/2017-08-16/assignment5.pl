a67c5c38f0dd8e1799fcbd8178619eeed9123f53
William Wennerström <william@optmzr.se>
2019-05-29 14:42:16 +0200

    exercises/prolog/2017-08-16: last part of assignment 5

--- exercises/prolog/2017-08-16/assignment5.pl
+++ exercises/prolog/2017-08-16/assignment5.pl
@@ -1,10 +1,27 @@
 /* vim: set syntax=prolog */
 
+% a)
  append([], List, List).
  append([Item|List1], List2, [Item|List3]) :-
 	 append(List1, List2, List3).
 
+% b)
  concat([], []).
  concat([List|Tail], Result) :-
 	 concat(Tail, NextLists),
 	 append(List, NextLists, Result).
+
+% c)
+% -? append(X, Y, [a, b, c, d]).
+% X = [],
+% Y = [a, b, c, d];
+% X = [a],
+% Y = [b, c, d];
+% X = [a, b],
+% Y = [c, d];
+% X = [a, b, c],
+% Y = [d];
+% X = [a, b, c, d],
+% Y = [];
+% false.
+% Y = [];
